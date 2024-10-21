6d4ccfe86b9e514b1bf4ebd41cc961eb752f70e5
Spencer Boucher <github@spencerboucher.com>
2024-10-21 12:36:40 -0600

    Change frontend run command to get env vars to work

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -49,8 +49,8 @@ export default function Login() {
   }, []);
 
   const handleLogin = (event) => {
-    console.log(API_HOST)
-    // window.location.href = `${API_HOST}/login`;
+    // console.log(API_HOST)
+    window.location.href = `${API_HOST}/login`;
   };
 
   useEffect(() => {
